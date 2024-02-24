40bf3bbf02f21eab6c50cd11bcc9e36388b7d72d
sryung <sryung1225@gmail.com>
2024-02-25 02:16:03 +0900

    [🔎 : feat] 페이지 이동하기 - Link의 경우 : href 속성 활용 - Button의 경우 : 커스텀훅 useRouter 활용하여 onClick 이벤트로 연결 - 바로 url 경로를 문자열로 이용하거나 pathname, query등의 router객체를 활용하거나 동일함

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,27 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  const code = "KOR";
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push("/search");
+  };
+  return (
+    <div>
+      Home Page
+      <div>
+        <button onClick={onClickButton}>Search 페이지로 이동</button>
+      </div>
+      <div>
+        <Link href={"/search"}>Search 페이지로 이동</Link>
+      </div>
+      <div>
+        {/* <Link href={`/country/${code}`}>{code} 페이지로 이동</Link> */}
+        <Link href={{ pathname: "/country/[code]", query: { code: code } }}>
+          {code} 페이지로 이동
+        </Link>
+      </div>
+    </div>
+  );
 }
