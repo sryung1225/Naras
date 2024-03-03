c21e384c58b85dc98ed013d6d451ab26ed72b918
sryung <sryung1225@gmail.com>
2024-03-03 22:56:29 +0900

    [✨ feat] SSR 방식 데이터 패치
    - getServerSideProps 내에서 axios data fetch 사용
    - React에서 fetch하는 것(CSR)은 페이지가 새로고침될 때 마다 브라우저 측에서 all이라는 api 호출. 첫 요청 html에는 데이터가 없음
    - Next에서는 어떠한 api도 브라우저에서는 호출하지 않음. 첫 요청 html에 데이터가 있음
    - 비교도구) 개발자도구 Next 패널의 Fetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,27 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { fetchCountries } from "@/api";
+import ICountry from "@/types/ICountry";
 
-export default function Home({ name }: { name: string }) {
-  // 이 곳에서 작성하는 코드는 server에서 먼저 실행. 동시에 브라우저에도 실행됨
-  const code = "KOR";
-  const router = useRouter();
-  const onClickButton = () => {
-    router.push("/search");
-  };
+interface IHome {
+  countries: ICountry[];
+}
 
-  useEffect(() => {
-    console.log("HOME MOUNT");
-  }, []); // server 측에만 console.log를 실행시키고 싶다면? useEffect를 이용해 마운트 될 떄 실행되도록
+export default function Home({ countries }: IHome) {
   return (
     <div>
-      Home Page
-      <p>{name}</p>
-      <div>
-        <button onClick={onClickButton}>Search 페이지로 이동</button>
-      </div>
-      <div>
-        <Link href={"/search"}>Search 페이지로 이동</Link>
-      </div>
-      <div>
-        {/* <Link href={`/country/${code}`}>{code} 페이지로 이동</Link> */}
-        <Link href={{ pathname: "/country/[code]", query: { code: code } }}>
-          {code} 페이지로 이동
-        </Link>
-      </div>
+      {countries.map((country) => (
+        <div key={country.code}>{country.commonName}</div>
+      ))}
     </div>
   );
 }
 
-// getServerSideProps를 통해 해당 페이지가 SSR 방식으로 적용하는 페이지가 됨
 export const getServerSideProps = async () => {
-  // SSR을 위해 서버측에서 페이지 컴포넌트에게 전달할 데이터를 설정하는 함수
-  // getServerSideProps의 내용은 오직 server 측에서만 실행됨
-  console.log("getServerSideProps Called"); // 화면 콘솔창(client)에는 뜨지 않음. 터미널에서만 출력됨
-  // window
+  // API 호출 코드가 필요함
+  const countries = await fetchCountries();
 
   return {
     props: {
-      name: "KOREA",
-    }, // props의 프로퍼티는 무조건 객체여야 함
+      countries,
+    },
   };
 };
---
+++ src/types/ICountry.ts
@@ -0,0 +1,9 @@
+export default interface ICountry {
+  code: string;
+  commonName: string;
+  flagEmoji: string;
+  flagImg: string;
+  capital: [string];
+  region: string;
+  population: number;
+}
