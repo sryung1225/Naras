2c78abbfb7fa8029cd24de3643cbdb5c95aed4c1
sryung <sryung1225@gmail.com>
2024-03-03 21:56:53 +0900

    [✨ feat] Home 페이지 컴포넌트 SSR 방식 적용
    - getServerSideProps를 이용하여 해당 페이지를 SSR 방식을 적용하는 페이지로 적용시킬 수 있음
    - getServerSideProps 내부의 코드는 server에서만 실행되는 코드. Home 내부의 코드는 server에서 우선. 그 다음 client에서 실행됨

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function Home() {
+export default function Home({ name }: { name: string }) {
+  // 이 곳에서 작성하는 코드는 server에서 먼저 실행. 동시에 브라우저에도 실행됨
   const code = "KOR";
   const router = useRouter();
   const onClickButton = () => {
     router.push("/search");
   };
+
+  useEffect(() => {
+    console.log("HOME MOUNT");
+  }, []); // server 측에만 console.log를 실행시키고 싶다면? useEffect를 이용해 마운트 될 떄 실행되도록
   return (
     <div>
       Home Page
+      <p>{name}</p>
       <div>
         <button onClick={onClickButton}>Search 페이지로 이동</button>
       </div>
@@ -25,3 +32,17 @@ export default function Home() {
     </div>
   );
 }
+
+// getServerSideProps를 통해 해당 페이지가 SSR 방식으로 적용하는 페이지가 됨
+export const getServerSideProps = async () => {
+  // SSR을 위해 서버측에서 페이지 컴포넌트에게 전달할 데이터를 설정하는 함수
+  // getServerSideProps의 내용은 오직 server 측에서만 실행됨
+  console.log("getServerSideProps Called"); // 화면 콘솔창(client)에는 뜨지 않음. 터미널에서만 출력됨
+  // window
+
+  return {
+    props: {
+      name: "KOREA",
+    }, // props의 프로퍼티는 무조건 객체여야 함
+  };
+};
