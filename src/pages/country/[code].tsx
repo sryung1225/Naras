92302968db8017b6f4731ab7a2b234fbc7c35471
sryung <sryung1225@gmail.com>
2024-03-03 23:59:25 +0900

    [✨ feat] Country 페이지 컴포넌트 (동적 페이지) SSG 방식 적용 - fallback: blocking

--- src/pages/country/[code].tsx
+++ src/pages/country/[code].tsx
@@ -7,6 +7,11 @@ export default function Country({ country }: { country: ICountry }) {
   const router = useRouter();
   const { code } = router.query;
 
+  if (!country) {
+    // fallback: "blocking" 중에 아래 return문에 접근해 에러를 발생하는 경우 방어
+    return <div>존재하지 않는 국가 입니다.</div>;
+  }
+
   return (
     <div>
       {code} {country.commonName} {country.officialName}
@@ -19,7 +24,8 @@ Country.Layout = SubLayout;
 export const getStaticPaths = async () => {
   return {
     paths: [{ params: { code: "KOR" } }, { params: { code: "ABW" } }], // 존재하는 동적 경로 안내
-    fallback: false, // fallback: false인 경우 위에서 명시하지 않은 경로를 접근하면 404를 노출
+    // fallback: false, // 위에서 명시하지 않은 경로를 접근하면 404를 노출
+    fallback: "blocking", // 위에서 명시하지 않은 경로를 접근하면 실시간으로 정적 페이지 생성. 이후 재접근 시 빠르게 페이지 가져옴
   };
 };
 
